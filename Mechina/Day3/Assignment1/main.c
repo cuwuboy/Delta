97697b292cf4d310a8b31fe4f21cc4ad75a21563
Yoav <malkayoav16@gmail.com>
2025-11-18 11:14:41 +0200

    Fix Targil 1

--- Mechina/Day3/Assignment1/main.c
+++ Mechina/Day3/Assignment1/main.c
@@ -1,3 +1,10 @@
+/*------------------------------------------------------
+* Filename: main.c
+* Description: Detect if a series represented by an array is
+increasing/decreasing/constant/messed up
+* Author: Yoav Malka
+-------------------------------------------------------*/
+
 #include <stdio.h>
 
 #define SIZE 15
@@ -5,22 +12,31 @@
 int main(){
     int arr[SIZE];
     
-    int num_incs, num_decs = 0;
-
+    int num_incs = 0 , num_decs = 0;
     int i=0;
+
     printf("Please enter %d values:\n",SIZE);
-    for(i=0;i<SIZE;i++) {
-        scanf("%d",&arr[i]);
+
+    for(i=0; i<SIZE; i++) {
+        if (scanf("%d", &arr[i]) == 1) {}
+        else {
+            printf("Invalid input! :(");
+            return -1;
+        }
 
         if(i>0){
             if(arr[i-1]<arr[i]) num_incs++;
             if(arr[i-1]>arr[i]) num_decs++;
         }
     }
-    if(num_incs == SIZE-1) printf("The array is increasing");
-    else if(num_decs == SIZE-1) printf("The array is decreasing");
-    else if (num_decs==0 && num_incs==0) printf("The array is constant");
-    else printf("The array is messed up");
+    if(num_incs == SIZE-1)
+        printf("The array is increasing");
+    else if(num_decs == SIZE-1)
+        printf("The array is decreasing");
+    else if (num_decs==0 && num_incs==0)
+        printf("The array is constant");
+    else
+        printf("The array is messed up");
     
     return 0;
 }
