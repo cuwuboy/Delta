49f103cbf00cc6a266c4011cfaf492f12396ff30
Yoav <malkayoav16@gmail.com>
2025-11-18 15:10:07 +0200

    Fix Targil2 Pointers

--- Mechina/Day3/Pointers1/main.c
+++ Mechina/Day3/Pointers1/main.c
@@ -15,7 +15,7 @@ int main(){
     for(int i=0;i<LEN;i++) {
         if(scanf(" %c", &chars[i])!=1) {
             printf("ERROR!");
-            return;
+            return -1;
         }
     }
 
@@ -23,15 +23,23 @@ int main(){
     for(int i=0;i<LEN;i++) {
         if(scanf(" %d", &ints[i])!=1) {
             printf("ERROR!");
-            return;
+            return -1;
         }
     }
     
+    printf("PRINT USING []:\n");
     for(int i=0;i<LEN;i++)
-        printf("INTS[%d] = %d | ADDRESS = %x\n", i, ints[i], &ints[i]);
+        printf("INTS[%d] = %d | ADDRESS = %p\n", i, ints[i], &ints[i]);
     printf("\n");
     for(int i=0;i<LEN;i++)
-        printf("CHARS[%d] = %c | ADDRESS = %x\n", i, chars[i], &chars[i]);
+        printf("CHARS[%d] = %c | ADDRESS = %p\n", i, chars[i], &chars[i]);
+
+    printf("PRINT USING *():\n");
+    for(int i=0;i<LEN;i++)
+        printf("INTS[%d] = %d | ADDRESS = %p\n", i, *(ints+i), (ints+i));
+    printf("\n");
+    for(int i=0;i<LEN;i++)
+        printf("CHARS[%d] = %c | ADDRESS = %p\n", i, *(chars+i), (chars+i));
 
     return 0;
 }
