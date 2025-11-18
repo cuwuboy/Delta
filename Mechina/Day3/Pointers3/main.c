e9234c1e2de50f8c7e379e565da269214a015dfd
Yoav <malkayoav16@gmail.com>
2025-11-18 15:21:11 +0200

    Add Targil3 Pointers

---
+++ Mechina/Day3/Pointers3/main.c
@@ -0,0 +1,36 @@
+/*------------------------------------------------------
+* Filename: main.c
+* Description: Const pointers
+* Author: Yoav Malka
+-------------------------------------------------------*/
+#include <stdio.h>
+
+/*------------------------------------------------------
+* Function Name - print_array_by_pointers
+*
+* Function Purpose - show the pointed values in array
+* Parameters –  None
+*
+* Return Values - None
+*
+* Author - Yoav Malka
+-------------------------------------------------------*/
+
+int main(){
+    const int channel = 5;
+    printf("Chanel no. %d\n", channel);
+
+    printf("Please enter new channel number:\n");
+    int new_channel;
+    if(scanf("%d",&new_channel)!=1){
+        printf("error :(");
+        return -1;
+    }
+
+    int* const ptr = &channel;
+    *ptr = new_channel;
+
+    printf("New channel is no. %d",channel);
+
+    return 0;
+}
